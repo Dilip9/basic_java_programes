b150e805c9e396e56eec8e2fabfe8bedc89efcb4
Dilip9 <Dilip.verma940805@gmail.com>
2022-12-11 19:54:31 +0530

    6. ReverseString

--- src/main/java/org/example/ReverseString.java
+++ src/main/java/org/example/ReverseString.java
@@ -5,6 +5,7 @@ public class ReverseString {
          int arr[] = {1,2,3,4,5,6};
          printArray(arr,6);
          reverseArray(arr,0,5);
+         reverseArrays(arr,0,5);
          System.out.println("Reversed array is \n");
          printArray(arr,6);
 
@@ -22,7 +23,18 @@ public class ReverseString {
          }
 
     }
+    //Another Approach to swap {1,2,3,4,5,6}={1,6,3,5,4,2}  where we are trying to swap +1 value into starting index.
+    private static void reverseArrays(int[] arr, int start, int end) {
+        int temp;
+        while(++start<end){
+            temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
 
+    }
     static void printArray(int arr[], int n){
          for (int i=0; i<n;i++){
              System.out.println(arr[i]+" ");
